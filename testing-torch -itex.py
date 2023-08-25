5d674ff386b616c4565760399d51e88993a46255
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-08-25 16:24:18 +0530

    itex

---
+++ testing-torch -itex.py
@@ -0,0 +1,85 @@
+#%%
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import intel_extension_for_pytorch as ipex
+
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torch.nn.functional as F 
+
+# Calculating notebook running time
+t0 = time.time()
+
+# Load the MNIST dataset
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+
+# Define the model architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 13 * 13, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(-1, 32 * 13 * 13)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training, p=0.2)
+        x = self.fc2(x)
+        return x
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = Net().to(device)
+
+# Define loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.RMSprop(model.parameters())
+model.train()
+model, optimizer = ipex.optimize(model, optimizer=optimizer)
+
+# Train the model
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(trainloader)}")
+
+print(f"Took {time.time()-t0:.2f} secs to run")
+
+# Evaluate the model
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print("Test accuracy:", correct/total)
+print(f"Took {time.time()-t0:.2f} secs to run")
+
+# %%
